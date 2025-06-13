49ef01760b226b1832efc2c5fbb392822bfe0c38
Leonam Miranda <leonam.miranda@br.ey.com>
2025-06-12 23:47:32 -0300

    Initial commit: project created with cookiecutter-data-science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repository contains the solution of clustering valuable clients in order to launch a loyalty program called insiders.',
+    author='leonamrsm@gmail.com',
+    license='MIT',
+)
